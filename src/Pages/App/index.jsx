0b52b4dd63d594044e66f264bbebf93a550d0808
C3M3Z4C4 <cmeza@mezadigital.com>
2023-05-27 22:34:44 -0600

    Implementando por primera vez context a la aplicacion

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,5 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom';
-
+import { ShoppingCartProvider } from '../../Context';
 import Home from '../Home';
 import MyAccount from '../MyAccount';
 import MyOrder from '../MyOrder';
@@ -27,10 +27,12 @@ const AppRoutes = () => {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-      <Navbar />
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
